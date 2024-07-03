d7ff7486fae436fad6f5fc99b39672757c0373fd
Tina Azimi <tazimiaraghi@scentregroup.com>
2024-07-03 17:21:40 +1000

    Add Collaboration

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Collaboration from "./components/Collaboration";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
